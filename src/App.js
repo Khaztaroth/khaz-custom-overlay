c60ed8edbf0134b7dce165c42222ef06bbe4778e
khaztaroth315 <astaroth503@gmail.com>
2023-02-18 13:52:02 -0300

    commented out some stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+// import { EmoteProvider } from "./api-requests/channelEmotes";
+// import DisplayBadges from "./api-requests/display-badges";
+
 import ChatMessages from "./chatMessages";
-import { EmoteProvider } from "./api-requests/channelEmotes";
 import { BadgeProvider } from "./api-requests/channelBadges";
-import DisplayBadges from "./api-requests/display-badges";
 
 import "./App.css"
 
