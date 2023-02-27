08dbed3c982ed7e87621279c6cad5bd35bb9cceb
khaztaroth315 <astaroth503@gmail.com>
2023-02-26 22:39:56 -0300

    removed unnecesary folder

--- _back/src/components/message-parts/Display-name.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export function DisplayName(props) {
-    return (
-       <strong className='user-name' style={{color: props.color}}>{props.user}: </strong>
-    )
-}
-
