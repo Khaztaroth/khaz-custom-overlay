40141470541d09e7db5ff9f152b32dfb489aada5
khaztaroth315 <astaroth503@gmail.com>
2023-03-06 00:22:43 -0300

    Fixed message scrolling

--- src/components/message-parts/Display-chat.js
+++ src/components/message-parts/Display-chat.js
@@ -17,22 +17,35 @@ export function DisplayChat() {
     const userID = localStorage.getItem("userID")
     // console.log(messages)
     
-   //Defining an effect that will scroll to the latest message
-  const messagesEndRef = useRef(null);
-  useEffect(() => {
-    messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
-  }, [messages]);
+//Defining an effect that will scroll to the latest message
+const messagesEndRef = useRef(null);
+useEffect(() => {
+  const container = messagesEndRef.current;
+  const shouldScroll =
+    container.scrollHeight > container.clientHeight + container.scrollTop;
+  if (shouldScroll) {
+    container.scrollTop = container.scrollHeight;
+  }
+}, [messages]);
 
-  //Message formatter, each element handles its segment through prop calls
-        return (
-            <div ref={messagesEndRef} style={{ overflowY: 'scroll', height: '100%', position:'relative', zIndex: '1'}}>
-                {messages.map((message, index) => (
-                <div key={index} className="message">
-                    <DisplayBadges badges={message.badges} id={userID}/>
-                    <DisplayName user={message.username} color={message.color} style={{ color: 'white' }}/>
-                    <DisplayEmotes emotes={message.emotes} message={message.message}/>
-                </div> ))}
-            </div>
-        )
- }
- 
+//Message formatter, each element handles its segment through prop calls
+return (
+  <div
+    className="chatContainer"
+    id="chatContainer"
+    ref={messagesEndRef}
+  >
+    {messages.map((message, index) => (
+      <div key={index} className="message">
+        <DisplayBadges badges={message.badges} id={userID} />
+        <DisplayName
+          user={message.username}
+          color={message.color}
+          style={{ color: "white" }}
+        />
+        <DisplayEmotes emotes={message.emotes} message={message.message} />
+      </div>
+    ))}
+  </div>
+)
+}
--- src/components/message-parts/Display-name.js
+++ src/components/message-parts/Display-name.js
@@ -1,8 +1,29 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export function DisplayName(props) {
+
+    let newColor = props.color
+
+    const [createColor, setCreateNewColor] = useState('#000000')
+
+    useEffect(() => {
+        const randomColor = () => {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random()*16)]
+            }
+            return color
+        }
+        setCreateNewColor(randomColor())
+    }, [])
+
+    if (props.color === null) {
+        newColor = createColor
+    }
+
     return (
-       <strong className='user-name' style={{color: props.color}}>{props.user}: </strong>
+       <strong className='user-name' style={{color: newColor}}>{props.user}: </strong>
     )
 }
 
