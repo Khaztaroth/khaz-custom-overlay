f629117fdded236feeb93e6e62dd3028f44b1296
khaztaroth315 <astaroth503@gmail.com>
2023-03-29 01:02:55 -0300

    minor fixes

--- src/components/pages/segments/overlay_games.js
+++ src/components/pages/segments/overlay_games.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 // import { UseSubscriptions } from "../../api-requests/subscription-handler";
 import { DisplayChat } from "../chat-formatting";
 
