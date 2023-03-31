a806a8c08e8c81cb78bbaa8cf9df316a60bd879a
khaztaroth315 <astaroth503@gmail.com>
2023-03-31 03:14:17 -0300

    code cleanup

--- src/components/message-parts/format-badges.js
+++ src/components/message-parts/format-badges.js
@@ -1,17 +1,16 @@
-//this code is better than before but it ain't good
-
 import DOMPurify from "dompurify";
-import { CHANNEL_KEY, DEFAULT_KEY, FetchBadges } from "../api-requests/badge-fetcher";
+import { CHANNEL_KEY, DEFAULT_KEY, USER_KEY, FetchBadges } from "../api-requests/badge-fetcher";
 import { RenderBadge } from "../hook/badge-rendered";
 
 export function DisplayBadges(props) {
 
+  const channel = localStorage.getItem(USER_KEY);
   const cachedChannelBadges = JSON.parse(localStorage.getItem(CHANNEL_KEY));
   const cachedDefaultBadges = JSON.parse(localStorage.getItem(DEFAULT_KEY));
   
   var badges = [];
 
-  if (cachedChannelBadges == null || cachedDefaultBadges == null) {
+  if (channel !== props.id || cachedChannelBadges == null || cachedDefaultBadges == null) {
     FetchBadges(props.id);
   } else {
     badges.push(cachedChannelBadges.badge_sets, cachedDefaultBadges.badge_sets)
