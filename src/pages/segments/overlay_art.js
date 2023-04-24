345abbeb87f083f83813ce3963da86d7064047ac
khaztaroth315 <astaroth503@gmail.com>
2023-04-23 22:00:47 -0400

    i accidentaly broke the art overlay fixed standalone chat when bellow 900px

--- src/pages/segments/overlay_art.js
+++ src/pages/segments/overlay_art.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Chat from "./chat";
 
 import "../style/gradients.css"
 import "../style/blocks.css"
 
-//sets up a url search in the current domain
-const params = new URLSearchParams(window.location.search)
-
-//looks for the value assinged to the "channel" header (url/thing?channel=)
-const chat = params.get('chat')
-
-var showChat = true
-
-if (chat === 'false') {
-    showChat = false
-}
-
 function ArtOverlay() {
+
+    //sets up a url search in the current domain
+    const params = new URLSearchParams(window.location.search)
+
+    //looks for the value assinged to the "channel" header (url/thing?channel=)
+    const chat = params.get('chat')
+
+    var showChat = true
+
+    if (chat === 'false') {
+        showChat = false
+    }
+
+    const showSub = useState(false)
+
+    const borderStyle = 
+        showSub? 
+        {borderImage: `conic-gradient(from var(--angle), var(--purple), var(--orange), var(--purple)) 1`}
+        : 
+        {borderImage: `conic-gradient(from var(--angle), var(--green1), var(--green2), var(--green1)) 1`} 
+
     return (
         <>
-            <div id='overlayBlock' className='block'>
-            
-            <div id="leftside" className="default">
-                <div id="chatBox" className="default" >{showChat ? <Chat /> : ""}</div>
-                <div id="webcam" className="default">
+            <div id='overlayBlock' 
+                className="frames"
+                style={borderStyle}
+            >
+            <div id="leftside" className="frames" style={borderStyle}>
+                <div id="chatBox" className="frames" style={borderStyle} >
+                    {showChat? <Chat /> : ""}
+                </div>
+                <div id="webcam" className="frames" style={borderStyle}>
            </div>
             </div>
-            <div id="rightside" className="default">
-                <div id="art_screen" className="default"></div>
+            <div id="rightside" className="frames" style={borderStyle}>
+                <div id="art_screen" className="frames" style={borderStyle}></div>
             </div>
-        
+
             </div>
         </>
 
